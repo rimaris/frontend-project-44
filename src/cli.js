148ab02d4046bc7167874893de4a9db595c021f5
mariyavasileva <marysolodova1998@yandex.ru>
2023-06-30 18:58:43 +0300

    added eslint; fixed code style

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-}
+const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+export default greetUser;
