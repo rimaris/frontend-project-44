6388de3d321a2888a1d5526a4a4f28901770cbb3
mariyavasileva <marysolodova1998@yandex.ru>
2023-07-01 13:07:57 +0300

    added brain-gcd game

---
+++ games/brain-gcd.js
@@ -0,0 +1,26 @@
+import askQuestion from '../src/index.js';
+
+const generateQuestion = () => {
+  const firstNumber = Math.floor(Math.random() * 100) + 1;
+  const secondNumber = Math.floor(Math.random() * 100) + 1;
+
+  const min = Math.min(firstNumber, secondNumber);
+  let maxDivisor = 1;
+  for (let i = 1; i <= min; i += 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      maxDivisor = i;
+    }
+  }
+
+  return {
+    question: `${firstNumber} ${secondNumber}`,
+    correctAnswer: maxDivisor,
+  };
+};
+
+const playOneRound = () => {
+  const question = generateQuestion();
+  return askQuestion(question.question, question.correctAnswer.toString());
+};
+
+export default playOneRound;
